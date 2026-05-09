0be786d6d239e17a132c529ac116897fa73caabb
Michał Olejniczak <blind.x6@gmail.com>
2026-05-09 22:59:12 +0200

    missing repo

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -18,7 +18,7 @@ module.exports = {
       user: 'debian',
       host: 'vps-ea2cb317.vps.ovh.net',
       ref: 'origin/main',
-      repo: 'git@github.com:TWOJE_REPO.git',
+      repo: 'git@github.com:pasikonik/dolinaharmonii_frontend.git',
       path: '/home/debian/apps/dolinaharmonii_frontend',
       'post-deploy': 'pnpm install && pnpm build && pm2 reload ecosystem.config.js --env production'
     }
